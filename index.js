c4c8ebb30652210a84965ebb8432ce0fa5a3710b
Hilarius Delta <hinodelpa97@gmail.com>
2022-01-06 11:34:31 +0700

    z

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const makeCommit = async (git, n) => {
     const x = random.int(0, 54)
     const y = random.int(0, 6)
     const DATE = moment()
-        .subtract(3, "y") //-3 years
+        .subtract(4, "y") //-4 yearss
         .add(1, "d")
         .add(x, "w")
         .add(y, "d")
